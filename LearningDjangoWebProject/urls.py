6afe2ca05e433e0c4ef254034a6734740a51cb3c
cedarchennn <a1249812431@gmail.com>
2019-10-11 11:02:30 +0800

    init

--- LearningDjangoWebProject/urls.py
+++ LearningDjangoWebProject/urls.py
@@ -1,9 +1,7 @@
-"""
-LearningDjangoWebProject URL Configuration
+"""LearningDjangoWebProject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,12 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
-# Uncomment next two lines to enable admin:
-#from django.contrib import admin
-#from django.urls import path
+from django.contrib import admin
+from django.urls import path
 
 urlpatterns = [
-    # Uncomment the next line to enable the admin:
-    #path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
 ]
